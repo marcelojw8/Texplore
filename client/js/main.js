1e10b7c2fa3ea1a2bfcc3fbdee5b3801ebf3af1b
mjwille <jantschwille@gmail.com>
2022-01-04 21:37:20 -0300

    Website Pulling Clubs' Data

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,25 @@
-alert("Hello!")
+const clubs_table = document.querySelector("#clubs_table");
+
+window.onload = (event) => {
+	// Browser assumes that '/clubs' is URL from which website got pulled
+	fetch('/clubs').then(res => {
+		// Because text of response is a 'Promise':
+		res.text().then((data) => {
+			data = data.split('\n');
+			console.log(data);
+			console.log(typeof(data));
+			// Headers
+			data[0].split(',').forEach(h => console.log(h));
+
+			console.log("---------------------");
+
+			// Club's Data
+			data.slice(1,data.length).forEach(row => {
+				row.split(',').forEach(piece => {
+					console.log(piece);
+				});
+				console.log("----");
+			});
+		});
+	});
+};
--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
+const CLUBS_FILE = path.join(__dirname, "../server/clubs.csv")
 const PORT = 3000;
 
 const app = express();
@@ -10,6 +12,11 @@ app.use(express.static(path.join(__dirname, "../client")));
 app.get('/', (req, res) => {
 	res.sendFile("index.html");
 });
+
+app.get('/clubs', (req, res) => {
+	var clubs_data = fs.readFileSync(CLUBS_FILE, 'utf-8');
+	res.send(clubs_data);
+})
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}...`);
