e5d6bd70d5d16633523f34ae7748afd1753bba32
mjwille <jantschwille@gmail.com>
2022-01-06 15:19:07 -0300

    Client getting data from Postgres

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,7 @@
-function addTable(headers, data) {
-	const clubs_table = document.querySelector("#clubs_table");
+const clubs_table = document.querySelector("#clubs_table");
+
+function createTableHeaders() {
+	const headers = ["Nome", "Fundação", "Títulos"];
 
 	let tableHeaderRow = document.createElement('tr');
 
@@ -10,30 +12,36 @@ function addTable(headers, data) {
 	});
 
 	clubs_table.append(tableHeaderRow);
+}
 
-	data.forEach(rawRow => {
-		const row = rawRow.split(',');
+function createTableData(clubs) {
+	clubs.forEach(club => {
 		let tableDataRow = document.createElement('tr');
 
-		row.forEach(info => {
-			let tableData = document.createElement('td');
-			tableData.innerHTML = info;
-			tableDataRow.append(tableData);
-		});
+		let tableNameData = document.createElement('td');
+		tableNameData.innerHTML = club.name;
+		tableDataRow.append(tableNameData);
+
+		let tableFoundationData = document.createElement('td');
+		tableFoundationData.innerHTML = club.foundation;
+		tableDataRow.append(tableFoundationData);
+
+		let tableTitlesData = document.createElement('td');
+		tableTitlesData.innerHTML = club.titles;
+		tableDataRow.append(tableTitlesData);
+
 		clubs_table.append(tableDataRow);
 	});
 }
-
 
 window.onload = (event) => {
 	// Browser assumes that '/clubs' is URL from which website got pulled
 	fetch('/clubs').then(res => {
 		// Because text of response is a 'Promise':
 		res.text().then((data) => {
-			data = data.split('\n');
-			headers = data[0].split(',');
-			data = data.slice(1,data.length)
-			addTable(headers, data);
+			createTableHeaders();
+			clubs = JSON.parse(data);
+			createTableData(clubs);
 		});
 	});
 };
